f26a19aacb6dcb5f310bc02564904d878c4f3ae0
tanthanhdev <tanthanhdev@gmail.com>
2021-12-21 06:08:47 +0700

    update config

--- src/services/callApiPublic.js
+++ src/services/callApiPublic.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://api-reviewmovie-dtu.herokuapp.com/api",//http://localhost:8000/
   headers: {
     "Content-type": "application/json"
   }
